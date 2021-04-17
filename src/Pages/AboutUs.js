a203a5f1b4d41e054385ee595877b0a0fdf57f56
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-04-17 18:38:22 -0400

    4.Faq Section

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from 'react'
 //page Components
 import AboutSection from '../Components/AboutSection';
 import ServicesSection from '../Components/ServicesSection';
+import  FaqSection from '../Components/FaqSection'
 
 const Aboutus = () => {
     return (
         <div>
             <AboutSection/>
             <ServicesSection/>
+            <FaqSection/>
         </div>
     )
 }
