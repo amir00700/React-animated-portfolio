bfc20bd74898b4c6a0ff8978cc760664a3c8c305
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-04-18 11:31:37 -0400

    FaQ section Style Added

--- src/Components/FaqSection.js
+++ src/Components/FaqSection.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+import {About} from '../styles';
 
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2> Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
                 <div className="answer">
+                    <div className="faq-line"></div>
                 <p>Lorem ipsum dolor sit amet.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, deserunt?</p>
                 </div>
@@ -15,6 +18,7 @@ const FaqSection = () => {
             <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                 <p>Lorem ipsum dolor sit amet.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, deserunt?</p>
                 </div>
@@ -23,6 +27,7 @@ const FaqSection = () => {
             <div className="question">
                 <h4>Different Payment Method</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                 <p>Lorem ipsum dolor sit amet.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, deserunt?</p>
                 </div>
@@ -31,14 +36,46 @@ const FaqSection = () => {
             <div className="question">
                 <h4>What Products do you Offer</h4>
                 <div className="answer">
+                <div className="faq-line"></div>
                 <p>Lorem ipsum dolor sit amet.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, deserunt?</p>
                 </div>
             </div>
             
-        </div>
+        </Faq>
     )
 }
+
+const Faq=styled(About)`
+display:block;
+span{
+    display:block;
+}
+
+h2{
+    padding-bottom:2rem;
+    font-weight:lighter;
+
+}
+.faq-line{
+    background:#cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width:100%;
+}
+ .question{
+     padding: 3rem 0rem;
+     cursor:pointer;   
+ }
+    .answer{
+        padding:2rem 0rem;
+        p{
+            padding:1rem 0rem;
+
+        }
+    }
+
+`
 
 export default FaqSection
 
