e3428715c135e40ffb7cf242896937143583d78f
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-04-18 11:59:24 -0400

    7.Nav added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 //import pages
 import AboutUs from "./Pages/AboutUs";
-
+import Nav from './Components/Nav'
 //Global style
 import GlobalStyle from './Components/GlobalStyle'
+
 
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <GlobalStyle/>
       <AboutUs/>
 
